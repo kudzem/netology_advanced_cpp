12152f351bce0e7e24d04a9563dd2b81cad6672e
Alexander Kokrov <kudzem@mail.ru>
2023-02-15 12:44:16 +0100

    More lambda usage

--- 1_new_features/lambda_demo.cpp
+++ 1_new_features/lambda_demo.cpp
@@ -8,7 +8,22 @@ struct Print
 };
 
 int main() {
-    std::vector<int> v{ 1,2,3,4,5,6,7,8,9,10 };
+    std::vector<int> v{ 1,2,3,4,5,6,7,8 };
     std::for_each(v.begin(), v.end(), [](int& val) { if (val % 2) val *= 3; });
     std::for_each(v.begin(), v.end(), Print());
+    std::cout << std::endl;
+
+    //auto f{ [&]() mutable { v.push_back(-1); } };
+    //f();
+
+    //int low = 4, high = 6;
+    //int cnt = count_if(v.begin(), v.end(), [&low, high](int& v)
+    //    { return v >= low && v <= high; });
+    //std::cout << cnt << std::endl;
+
+    //std::for_each(v.begin(), v.end(), Print());
+
+    //auto mymod{ [](const int& a) -> double { if (a < 0) return 0.0; return a; } };
+    //std::cout << mymod(-4) << std::endl;
+
 }
