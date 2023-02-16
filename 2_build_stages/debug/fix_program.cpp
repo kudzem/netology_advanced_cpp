328996dcd3bd50849c650960e75970e3710eb908
Alexander Kokrov <kudzem@mail.ru>
2023-02-16 23:28:49 +0100

    Bug fixes

--- 2_build_stages/debug/fix_program.cpp
+++ 2_build_stages/debug/fix_program.cpp
@@ -6,26 +6,20 @@ struct point {
 	double m_y;
 	point(double x, double y) {
 		m_x = x;
-		y = y;
+		m_y = y;
 	}
-}
+};
 
 void print_point(const point& point_object) {
 	std::cout << "x:" << point_object.m_x << ", y: "
 		<< point_object.m_y << std::endl;
 }
 
-int Main()
+int main()
 {
-	int i;
-	for (i = 0; i < 5; i++); {
+	for (int i = 0; i < 5; ++i) {
 		point my_point(i, 2 * i);
 		print_point(my_point);
 	}
 	return 0;
-}
-
-void print_point(const point& point_object) {
-	std::cout << "x:" << point_object.m_x << ", y: "
-		<< point_object.m_y << std::endl;
 }
