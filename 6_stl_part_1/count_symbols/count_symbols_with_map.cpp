afe622029e3a8d397037d4b8d950bd8be0aa5006
Alexander Kokrov <kudzem@mail.ru>
2023-03-07 22:04:16 +0100

    Change map<int, list<char>> to multimap<int,char>

--- 6_stl_part_1/count_symbols/count_symbols_with_map.cpp
+++ 6_stl_part_1/count_symbols/count_symbols_with_map.cpp
@@ -2,25 +2,23 @@
 #include <algorithm>
 #include <string>
 #include <map>
-#include <list>
+#include <utility>
 #include <functional>
 
 class CountSymbols {
 	std::map<char, size_t> _symbol2quantity;
-	std::map<size_t, std::list<char>, std::greater<size_t>> _quantity2symbol;
+	std::multimap<size_t, char, std::greater<size_t>> _quantity2symbol;
 public:
 
 	CountSymbols(const std::string& str) {
 		for_each(str.cbegin(), str.cend(), [this](char symbol) { ++_symbol2quantity[symbol]; });
 		for_each(_symbol2quantity.cbegin(), _symbol2quantity.cend(), [this](const std::pair<char,size_t>& data) {
-			_quantity2symbol[data.second].push_back(data.first); });
+			_quantity2symbol.insert(std::make_pair(data.second, data.first)); });
 	}
 
 	void Print() const {
 		for (const auto& item : _quantity2symbol) {
-			for (const auto& symbol : item.second) {
-				std::cout << symbol << ": " << item.first << std::endl;
-			}
+			std::cout << item.second << ": " << item.first << std::endl;
 		}
 	}
 
