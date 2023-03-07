70373f178d532edcf2881e3e8771ff8b40d73625
Alexander Kokrov <kudzem@mail.ru>
2023-03-07 23:12:25 +0100

    Make functor in correct STL way. Hope so =)

--- 5_templates_n_functors/functors/demo_functors.cpp
+++ 5_templates_n_functors/functors/demo_functors.cpp
@@ -4,43 +4,43 @@
 #include <numeric>
 #include <string>
 
-template <class C, class T, int N>
+template <class T, int N>
 class CountMultiplesOf {
 	T _sum;
 	size_t _quantity;
 public:
 	CountMultiplesOf(): _sum(0), _quantity(0) {}
 
-	void operator() (const C & data) {
-		std::for_each(data.begin(), data.end(),
-			          [this](const auto& item) 
-			          { if (item % N == 0) 
-		                { _sum += item; ++_quantity; }; 
-			          });
+	void operator() (T new_entry) {
+		if (std::fmod(new_entry, N) == 0) {
+			_sum += new_entry; ++_quantity;
+		};
 	}
 
 	T get_sum() const { return _sum; }
 	size_t get_count() const { return _quantity; }
 };
 
-typedef CountMultiplesOf<std::vector<int>, double, 3> CountMultiplesOf3;
-typedef CountMultiplesOf<std::vector<int>, double, 5> CountMultiplesOf5;
+typedef CountMultiplesOf<double, 3> CountMultiplesOf3;
+typedef CountMultiplesOf<int, 5> CountMultiplesOf5;
 
 int main() 
 {
 	CountMultiplesOf3 Mof3;
 	std::vector v{ 1,2,3,4,5 };
 	std::vector v2{ 11,12,13,14,15 };
-	Mof3(v);
-	Mof3(v2);
+	Mof3 = std::for_each(v.cbegin(), v.cend(), Mof3);
+	Mof3 = std::for_each(v2.cbegin(), v2.cend(), Mof3);
 
+	std::cout << "Multiples of 3" << std::endl;
 	std::cout << "Sum = " << Mof3.get_sum() << std::endl;
 	std::cout << "Count = " << Mof3.get_count() << std::endl;
 
 	CountMultiplesOf5 Mof5;
-	Mof5(v);
-	Mof5(v2);
+	Mof5 = std::for_each(v.cbegin(), v.cend(), Mof5);
+	Mof5 = std::for_each(v2.cbegin(), v2.cend(), Mof5);
 
+	std::cout << "Multiples of 5" << std::endl;
 	std::cout << "Sum = " << Mof5.get_sum() << std::endl;
 	std::cout << "Count = " << Mof5.get_count() << std::endl;
 
